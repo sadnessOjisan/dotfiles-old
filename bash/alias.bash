13ee51d14e48d8c4b61d10d98a6553c375f0ae2d
sadnessOjisan <sadness.ojisan@gmail.com>
2020-05-27 10:52:48 +0900

    bash & fish (#15)
    * zsh env
    
    * add fish
    
    * bash
    
    * bash
    
    * add git alias

---
+++ bash/alias.bash
@@ -0,0 +1,19 @@
+# .bashrc を読み直す
+alias br="source ~/.bashrc"
+
+###
+### Git系
+###
+
+alias g='git'
+alias gb='git branch'
+alias gbc='git checkout -b'
+alias gc='git commit --verbose'
+alias gco='git checkout'
+alias gia='git add'
+alias gm='git merge'
+alias gp='git push'
+alias gfm='git pull'
+
+# GitHubを表示
+alias hb="hub browse"
